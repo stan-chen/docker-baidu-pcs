a6fd2824f3a9cb119008ccdf30b1fc2ce93eba99
chenxuefei <chenxuefei_pp@163.com>
2017-04-28 13:49:12 +0800

    小范围修改

--- download.sh
+++ download.sh
@@ -2,6 +2,10 @@
 
 REMOTE_DIR_NAME = $REMOTE_NAME
 
+if [ ! -d "/download$REMOTE_DIR_NAME" ]; then
+  mkdir -p /download$REMOTE_DIR_NAME
+fi
+
 pcs synch -cd /download$REMOTE_DIR_NAME $REMOTE_DIR_NAME
 
 
